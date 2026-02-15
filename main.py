3c45de09e25fa70c2a3af2303b740aebcc601b14
riteshkundara <23f2002447@ds.study.iitm.ac.in>
2026-02-15 09:07:55 +0530

    Enable CORS for public access

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import os
 
 PORT = int(os.environ.get("PORT", 8000))
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import time
 import hashlib
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Simple cache
 cache = {}
